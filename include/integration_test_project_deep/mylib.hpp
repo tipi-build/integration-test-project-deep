df4f3519e49dc65b72c160105e639e6466bcf888
Yannic Staudt <yannic@tipi.build>
2023-11-11 14:05:01 +0100

    :wrench: alternatve return value from branch-b

--- include/integration_test_project_deep/mylib.hpp
+++ include/integration_test_project_deep/mylib.hpp
@@ -5,6 +5,6 @@ namespace tipi::integration_tests::project_deep {
     using namespace std::string_literals;
 
     inline std::string library_call() {
-        return "tipi-build/integration_test_project_deep@main::library_call()"s; 
+        return "tipi-build/integration_test_project_deep@branch-b::library_call()"s; 
     }
 }
