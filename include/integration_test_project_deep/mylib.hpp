df397cec7ea816b412a8acbdd07a398c1c5cd209
Yannic Staudt <yannic@tipi.build>
2023-11-11 14:39:18 +0100

    :wrench: with "as-submodule" marker

--- include/integration_test_project_deep/mylib.hpp
+++ include/integration_test_project_deep/mylib.hpp
@@ -5,6 +5,6 @@ namespace tipi::integration_tests::project_deep {
     using namespace std::string_literals;
 
     inline std::string library_call() {
-        return "tipi-build/integration_test_project_deep@main::library_call()"s; 
+        return "tipi-build/integration_test_project_deep@as-submodule::library_call()"s; 
     }
 }
